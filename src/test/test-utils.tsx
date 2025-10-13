052c71a5feb414de1527241482cdf3868d69e497
Eduardo Saatkamp <eduardosaatkamp@gmail.com>
2025-10-12 21:58:36 -0300

    Removido projeto atual pra adicionar novo com backend local

--- src/test/test-utils.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-import { render } from '@testing-library/react';
-
-export function renderWithI18n(ui: ReactNode) {
-  return render(ui);
-}
-
--- supabase/migrations/20250915190000_76f31ab6-1c0a-4dfc-9f71-7d2cde7f9616.sql
+++
@@ -1,4 +0,0 @@
--- Remove a coluna message de public.help_requests, pois o campo não será mais utilizado.
-
-ALTER TABLE public.help_requests
-DROP COLUMN message;
